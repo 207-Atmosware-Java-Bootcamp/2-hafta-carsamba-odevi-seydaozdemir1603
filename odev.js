625be2d695f51b4f95603ba8cc34922535962839
seydaozdemir1603 <seydaozdemir1603@gmail.com>
2022-06-01 13:30:16 +0300

    new commit

---
+++ odev.js
@@ -0,0 +1,15 @@
+/* var n1, n2;
+n1 = Number(prompt("1.sayı: "));
+n2 = Number(prompt("2. sayı: "));
+
+
+document.write("En büyüğü: "+ Math.max(n1,n2)+"<br/>");
+document.write("En küçüğü: "+ Math.min(n1,n2)+"<br/>");
+document.write("en küçüğü karekök: "+Math.sqrt(Math.abs(Math.min(n1,n2)))  +"<br/>")
+document.write("en küçüğü karekökünün üste yuvarlı: "+Math.ceil(Math.sqrt(Math.abs(Math.min(n1,n2))))    +"<br/>");
+document.write("en büyüğü mutlak: " + Math.abs(Math.max(n1,n2)) + "<br>");
+document.write("en büyüğü alt taban en küçüğü üst taban: "+
+  Math.pow(Math.abs(Math.max(n1,n2)), Math.abs(Math.min(n1,n2)))+ "<br/>");
+
+*/
+
